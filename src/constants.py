62cf5041f492c7177895096fe850c09cf4385a21
Niall Asher <niallasher@protonmail.com>
2021-11-28 23:49:13 +1300

    add instance_admin to accountattributers

--- src/constants.py
+++ src/constants.py
@@ -11,4 +11,7 @@ class AccountAttributes(Enum):
   BANNED = 3
   PRIVATE = 4
   BETA_TESTER = 5
-  OG = 6
+  OG = 6 # user since socialshare pre-1.0. what a legend.
+  INSTANCE_ADMIN = 7  # can control webui when it's added.
+                      # might even get a tab in the mobile app for some cleanup tasks?
+                      # basically could be aliases to GOD_EMPEROR, but that's a bit of a stretch.
