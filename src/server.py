75ab24c0de20a72b769296389ea50173d7a53d4f
Niall Asher <niallasher@protonmail.com>
2021-11-29 00:10:18 +1300

    autopep8 server.py

--- src/server.py
+++ src/server.py
@@ -4,16 +4,19 @@ from configutil import config
 
 app = Flask(__name__)
 
+
 @app.get('/')
 def landing_page():
-  return render_template('server_landing.html')
+    return render_template('server_landing.html')
+
 
 if __name__ == '__main__':
-  if config.debug.auto_reload_templates:
-    print("Debug: templates auto reloading")
-    app.config['TEMPLATES_AUTO_RELOAD'] = True
-  if config.debug.zero_max_file_age:
-    print("Debug: max file age is zero")
-    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+    if config.debug.auto_reload_templates:
+        print("Debug: templates auto reloading")
+        app.config['TEMPLATES_AUTO_RELOAD'] = True
+    if config.debug.zero_max_file_age:
+        print("Debug: max file age is zero")
+        app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-  app.run(host=config.network.host, port=config.network.port, debug=config.debug.enable_flask_debug_mode)
+    app.run(host=config.network.host, port=config.network.port,
+            debug=config.debug.enable_flask_debug_mode)
