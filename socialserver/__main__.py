c29d36bc29aaf9f996f3cf3ea9dac277cda0fcdc
niall <niall@asher.sh>
2021-12-01 00:54:11 +1300

    move cli into it's own subfolder
    also remove old run_debug from __main__

--- socialserver/__main__.py
+++ socialserver/__main__.py
@@ -1,17 +1,7 @@
 from argparse import ArgumentParser
 from socialserver.util.config import config
 from socialserver import application
-from socialserver.cli import cli
-
-
-def run_debug():
-    if config.debug.auto_reload_templates:
-        print("Debug: templates auto reloading")
-        application.config['TEMPLATES_AUTO_RELOAD'] = True
-    if config.debug.zero_max_file_age:
-        print("Debug: max file age is zero")
-        application.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-
+from socialserver.cli.cli import cli
 
 if __name__ == "__main__":
     # use the cli if calling the module directly,
