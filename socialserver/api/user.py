dbf1c4d7379d8461cc7aa498e55f4b886c9d6010
niall <niall@asher.sh>
2021-12-03 17:21:49 +1300

    implement user signup, no totp for now.

--- socialserver/api/user.py
+++ socialserver/api/user.py
@@ -1,8 +1,9 @@
-from re import I
+from datetime import datetime
+import re
 from socialserver.db import DbUser
 from flask_restful import Resource, reqparse
-from socialserver.constants import ErrorCodes
-from socialserver.util.auth import get_username_from_token
+from socialserver.constants import BIO_MAX_LEN, DISPLAY_NAME_MAX_LEN, MAX_PASSWORD_LEN, MIN_PASSWORD_LEN, USERNAME_MAX_LEN, ErrorCodes, REGEX_USERNAME_VALID
+from socialserver.util.auth import generate_salt, get_username_from_token, hash_password
 from pony.orm import db_session
 
 
@@ -46,3 +47,63 @@ class UserInfo(Resource):
             "profile_picture": pfp,
             "header": header
         }, 200
+
+
+class User(Resource):
+
+    @db_session
+    def post(self):
+
+        parser = reqparse.RequestParser()
+        parser.add_argument('display_name', type=str, required=True)
+        parser.add_argument('username', type=str, required=True)
+        parser.add_argument('password', type=str, required=True)
+        parser.add_argument('bio', type=str)
+        # birthday is unimplemented for now.
+        # this will have to be turned into a datetime.date,
+        # and there are a lot of considerations. probably
+        # a feature to do *after* everything else works!
+        # parser.add_argument('birthday', type=str)
+        args = parser.parse_args()
+
+        # all this validation should be done clientside,
+        # but we're replicating here in case of an error on the client
+        # or a bad actor. hence the ux doesn't have to be amazing,
+        # and it doesn't really matter if we don't check everything
+        # before exiting. we just report the first issue we find and
+        # return.
+
+        # usernames can only have a-z, A-Z, 0-9 or _, and must be between 1 and 20 chars
+        if not bool(re.match(REGEX_USERNAME_VALID, args['username'])):
+            return {"error": ErrorCodes.USERNAME_INVALID.value}, 400
+
+        existing_user = DbUser.get(username=args['username'])
+        if existing_user is not None:
+            return {"error": ErrorCodes.USERNAME_TAKEN.value}, 400
+
+        if args['bio'] != None and len(args['bio']) >= BIO_MAX_LEN:
+            return {"error": ErrorCodes.BIO_NON_CONFORMING.value}, 400
+
+        if len(args['display_name']) >= DISPLAY_NAME_MAX_LEN:
+            return {"error": ErrorCodes.DISPLAY_NAME_NON_CONFORMING.value}, 400
+
+        password_ok = len(args['password']) >= MIN_PASSWORD_LEN and len(
+            args['password']) <= MAX_PASSWORD_LEN
+        if not password_ok:
+            return {"error": ErrorCodes.PASSWORD_NON_CONFORMING.value}, 400
+
+        salt = generate_salt()
+        password = hash_password(args['password'], salt)
+
+        DbUser(
+            display_name=args['display_name'],
+            username=args['username'],
+            password_hash=password,
+            password_salt=salt,
+            creation_time=datetime.now(),
+            is_legacy_account=False,
+            account_attributes=[],
+            bio=args['bio'] if args['bio'] != None else ""
+        )
+
+        return {}, 201
