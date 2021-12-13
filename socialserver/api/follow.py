525e3405a83e4d981ff74d50a61cfa2fb4e87406
niall <niall@asher.sh>
2021-12-14 12:03:26 +1300

    add followers support

---
+++ socialserver/api/follow.py
@@ -0,0 +1,75 @@
+from datetime import datetime
+from flask_restful import Resource, reqparse
+from werkzeug.wrappers import request
+from socialserver.db import DbUser, DbFollow
+from socialserver.util.auth import get_username_from_token
+from socialserver.constants import ErrorCodes
+from pony.orm import db_session
+
+
+class Follow(Resource):
+
+    @db_session
+    def post(self):
+
+        parser = reqparse.RequestParser()
+        parser.add_argument("access_token", type=str, required=True)
+        # the username to follow
+        parser.add_argument("username", type=str, required=True)
+        args = parser.parse_args()
+
+        requesting_user = get_username_from_token(args['access_token'])
+        if requesting_user is None:
+            return {"error": ErrorCodes.TOKEN_INVALID.value}, 401
+
+        requesting_user_db = DbUser.get(username=requesting_user)
+
+        user_to_follow = DbUser.get(username=args['username'])
+        if user_to_follow is None:
+            return {"error": ErrorCodes.USERNAME_NOT_FOUND.value}, 404
+        if user_to_follow is requesting_user_db:
+            return {"error": ErrorCodes.CANNOT_FOLLOW_SELF.value}, 400
+
+        existing_follow = DbFollow.get(
+            user=requesting_user_db,
+            following=user_to_follow
+        )
+        if existing_follow is not None:
+            return {"error": ErrorCodes.FOLLOW_ALREADY_EXISTS.value}, 400
+
+        DbFollow(
+            user=requesting_user_db,
+            following=user_to_follow,
+            creation_time=datetime.now()
+        )
+
+        return {}, 201
+
+    @db_session
+    def delete(self):
+
+        parser = reqparse.RequestParser()
+        parser.add_argument("access_token", type=str, required=True)
+        parser.add_argument("username", type=str, required=True)
+        args = parser.parse_args()
+
+        requesting_user = get_username_from_token(args['access_token'])
+        if requesting_user is None:
+            return {"error": ErrorCodes.TOKEN_INVALID.value}, 401
+
+        requesting_user_db = DbUser.get(username=requesting_user)
+
+        user_to_unfollow = DbUser.get(username=args['username'])
+        if user_to_unfollow is None:
+            return {ErrorCodes.USERNAME_NOT_FOUND.value}, 404
+
+        existing_follow = DbFollow.get(
+            user=requesting_user_db,
+            following=user_to_unfollow
+        )
+
+        if existing_follow is None:
+            return {ErrorCodes.CANNOT_FIND_FOLLOW_ENTRY.value}, 404
+
+        existing_follow.delete()
+        return {}, 204
