28857fe181ffd38fe418ff12494782f1f78f131c
niall <niall@asher.sh>
2021-12-19 20:33:20 +1300

    fix serverinfo not being a class

--- socialserver/api/v2/info.py
+++ socialserver/api/v2/info.py
@@ -3,8 +3,10 @@ from socialserver.constants import SERVER_VERSION
 from socialserver.util.config import config
 
 
-def ServerInfo(Resource):
+class ServerInfo(Resource):
 
+    # Ignore the ide. This shouldn't be a static method.
+    # It doesn't seem to work with flask-restful when it is.
     def get(self):
 
         reg_type = 0
