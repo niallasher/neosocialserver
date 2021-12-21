587b6205c38c06b69e88078867629fc4f36d688b
niall <niall@asher.sh>
2021-12-22 03:34:11 +1300

    add userpatch username change tests

--- socialserver/tests/api2/test_user.py
+++ socialserver/tests/api2/test_user.py
@@ -1,8 +1,5 @@
-from socialserver.tests.util import test_db, server_address, create_test_user_api2, test_db_with_user
-from datetime import datetime
+from socialserver.tests.util import test_db, server_address, test_db_with_user
 import requests
-from socialserver.db import create_test_db, define_entities
-from socialserver import api
 from pony.orm import db_session
 from socialserver.constants import ErrorCodes, BIO_MAX_LEN
 from secrets import token_urlsafe
@@ -20,6 +17,7 @@ def test_create_user(test_db, server_address, monkeypatch):
     print(user_creation_request.json())
     assert user_creation_request.status_code.__int__() == 201
     test_db.drop_all_tables(with_all_data=True)
+
 
 def test_create_user_with_bio(test_db, server_address, monkeypatch):
     monkeypatch.setattr("socialserver.api.v2.user.db", test_db)
@@ -82,6 +80,7 @@ def test_create_user_username_too_long(test_db, server_address, monkeypatch):
     assert invalid_req.status_code == 400
     assert invalid_req.json()['error'] == ErrorCodes.USERNAME_INVALID.value
 
+
 def test_delete_user(test_db_with_user, server_address, monkeypatch):
     monkeypatch.setattr("socialserver.api.v2.user.db", test_db_with_user.get('db'))
     monkeypatch.setattr("socialserver.util.auth.db", test_db_with_user.get('db'))
@@ -93,3 +92,82 @@ def test_delete_user(test_db_with_user, server_address, monkeypatch):
                               })
     print(del_req.json())
     assert del_req.status_code == 200
+
+
+def test_delete_user_invalid_password(test_db_with_user, server_address, monkeypatch):
+    monkeypatch.setattr("socialserver.api.v2.user.db", test_db_with_user.get('db'))
+    monkeypatch.setattr("socialserver.util.auth.db", test_db_with_user.get('db'))
+    print(test_db_with_user.get('access_token'))
+    del_req = requests.delete(f"{server_address}/api/v2/user",
+                              json={
+                                  "access_token": test_db_with_user.get('access_token'),
+                                  "password": "defo_not_the_right_password"
+                              })
+    print(del_req.json())
+    assert del_req.status_code == 401
+    assert del_req.json()['error'] == ErrorCodes.INCORRECT_PASSWORD.value
+
+
+def test_delete_user_invalid_token(test_db_with_user, server_address, monkeypatch):
+    monkeypatch.setattr("socialserver.api.v2.user.db", test_db_with_user.get('db'))
+    monkeypatch.setattr("socialserver.util.auth.db", test_db_with_user.get('db'))
+    print(test_db_with_user.get('access_token'))
+    del_req = requests.delete(f"{server_address}/api/v2/user",
+                              json={
+                                  "access_token": 'not_even_valid_form',
+                                  "password": test_db_with_user.get('password')
+                              })
+    print(del_req.json())
+    assert del_req.status_code == 401
+    assert del_req.json()['error'] == ErrorCodes.TOKEN_INVALID.value
+
+
+def test_update_username(test_db_with_user, server_address, monkeypatch):
+    monkeypatch.setattr("socialserver.api.v2.user.db", test_db_with_user.get('db'))
+    monkeypatch.setattr("socialserver.util.auth.db", test_db_with_user.get('db'))
+    print(test_db_with_user.get('access_token'))
+    del_req = requests.patch(f"{server_address}/api/v2/user",
+                             json={
+                                 "access_token": test_db_with_user.get('access_token'),
+                                 "username": "validusername"
+                             })
+    print(del_req.json())
+    assert del_req.status_code == 200
+    assert del_req.json()['username'] == 'validusername'
+
+
+def test_update_username_invalid(test_db_with_user, server_address, monkeypatch):
+    monkeypatch.setattr("socialserver.api.v2.user.db", test_db_with_user.get('db'))
+    monkeypatch.setattr("socialserver.util.auth.db", test_db_with_user.get('db'))
+    print(test_db_with_user.get('access_token'))
+    del_req = requests.patch(f"{server_address}/api/v2/user",
+                             json={
+                                 "access_token": test_db_with_user.get('access_token'),
+                                 "username": "defonotavalidusernamecuzitswaytoolong"
+                             })
+    print(del_req.json())
+    assert del_req.status_code == 400
+    assert del_req.json()['error'] == ErrorCodes.USERNAME_INVALID.value
+
+
+def test_update_username_already_exists(test_db_with_user, server_address, monkeypatch):
+    monkeypatch.setattr("socialserver.api.v2.user.db", test_db_with_user.get('db'))
+    monkeypatch.setattr("socialserver.util.auth.db", test_db_with_user.get('db'))
+
+    # create a second user, so we can try to steal its name
+    requests.post(f"{server_address}/api/v2/user",
+                  json={
+                      "display_name": "Test User 2",
+                      "username": "test2",
+                      "password": "password",
+                  })
+
+    print(test_db_with_user.get('access_token'))
+    del_req = requests.patch(f"{server_address}/api/v2/user",
+                             json={
+                                 "access_token": test_db_with_user.get('access_token'),
+                                 "username": "test2"
+                             })
+    print(del_req.json())
+    assert del_req.status_code == 400
+    assert del_req.json()['error'] == ErrorCodes.USERNAME_TAKEN.value
