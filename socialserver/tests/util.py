a29171de3743bfd8ae9fa5933ded8180f79556b4
niall <niall@asher.sh>
2021-12-22 03:25:10 +1300

    add fixture that creates a test database with a user already created + a session for that user

--- socialserver/tests/util.py
+++ socialserver/tests/util.py
@@ -11,7 +11,7 @@ UA = "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.3
      "Mobile/15A372 Safari/604.1 "
 
 """
-    mem_db
+    test_db
     pytest fixture that creates an in-memory database with nothing in it
 """
 
@@ -57,7 +57,44 @@ def create_test_user_api2(addr, username="test", password="test", display_name="
     )
 
 """
-    get_session_token_api2
+    test_db_with_user
     
-    logs in and returns the token using api2
+    creates a testdb with a user and a session
 """
+
+@db_session
+@pytest.fixture
+def test_db_with_user():
+    test_db = db.create_test_db()
+    salt = generate_salt()
+    password = "password"
+    password_hash = hash_password(password, salt)
+    test_db.User(
+        username="test",
+        display_name="test",
+        password_hash=password_hash,
+        password_salt=salt,
+        creation_time=datetime.now(),
+        account_attributes=[0],
+        bio="",
+        account_approved=True,
+        is_legacy_account=False
+    )
+    # we're saving here, so we can create a session using this user
+    commit()
+    secret = generate_key()
+    test_db.UserSession(
+        access_token_hash=secret.hash,
+        user=test_db.User.get(username="test"),
+        creation_ip="127.0.0.1",
+        creation_time=datetime.now(),
+        last_access_time=datetime.now(),
+        user_agent=UA
+    )
+    commit()
+    return {
+        "db": test_db,
+        "username": "test",
+        "password": "password",
+        "access_token": secret.key
+    }
