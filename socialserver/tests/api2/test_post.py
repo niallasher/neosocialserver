1295765a3ea740794b308aba07a4794a1ce1c477
niall <niall@asher.sh>
2021-12-24 17:00:23 +1300

    add single post tests

--- socialserver/tests/api2/test_post.py
+++ socialserver/tests/api2/test_post.py
@@ -1,5 +1,6 @@
 # noinspection PyUnresolvedReferences
-from socialserver.tests.util import test_db_with_user, server_address, create_user_session_with_request
+from socialserver.tests.util import test_db_with_user, server_address, create_post_with_request
+from socialserver.constants import ErrorCodes
 import requests
 
 
@@ -9,11 +10,104 @@ def test_create_single_post(test_db_with_user, server_address, monkeypatch):
     monkeypatch.setattr("socialserver.util.auth.db", test_db_with_user.get('db'))
 
     r = requests.post(f"{server_address}/api/v2/post/single",
-                  json={
-                      "access_token": test_db_with_user.get('access_token'),
-                      "text_content": "Test Post"
-                  })
+                      json={
+                          "access_token": test_db_with_user.get('access_token'),
+                          "text_content": "Test Post"
+                      })
 
     assert r.status_code == 200
     # blank database, so this should be post id 1.
     assert r.json()['post_id'] == 1
+
+
+def test_create_single_post_missing_args(test_db_with_user, server_address, monkeypatch):
+    monkeypatch.setattr("socialserver.api.v2.user.db", test_db_with_user.get('db'))
+    monkeypatch.setattr("socialserver.api.v2.post.db", test_db_with_user.get('db'))
+    monkeypatch.setattr("socialserver.util.auth.db", test_db_with_user.get('db'))
+
+    r = requests.post(f"{server_address}/api/v2/post/single",
+                      json={})
+
+    assert r.status_code == 400
+
+
+def test_create_single_post_invalid_access_token(test_db_with_user, server_address, monkeypatch):
+    monkeypatch.setattr("socialserver.api.v2.user.db", test_db_with_user.get('db'))
+    monkeypatch.setattr("socialserver.api.v2.post.db", test_db_with_user.get('db'))
+    monkeypatch.setattr("socialserver.util.auth.db", test_db_with_user.get('db'))
+
+    r = requests.post(f"{server_address}/api/v2/post/single",
+                      json={
+                          "access_token": "invalid_access_token",
+                          "text_content": "Test Post"
+                      })
+
+    assert r.status_code == 401
+    assert r.json()['error'] == ErrorCodes.TOKEN_INVALID.value
+
+
+def test_get_single_post(test_db_with_user, server_address, monkeypatch):
+    monkeypatch.setattr("socialserver.api.v2.user.db", test_db_with_user.get('db'))
+    monkeypatch.setattr("socialserver.api.v2.post.db", test_db_with_user.get('db'))
+    monkeypatch.setattr("socialserver.util.auth.db", test_db_with_user.get('db'))
+
+    new_post_id = create_post_with_request(server_address,
+                                           test_db_with_user.get('access_token'))
+
+    r = requests.get(f"{server_address}/api/v2/post/single",
+                     json={
+                         "access_token": test_db_with_user.get("access_token"),
+                         "post_id": new_post_id
+                     })
+
+    assert r.status_code == 201
+    assert r.json()['post']['id'] == new_post_id
+    assert r.json()['user']['username'] == test_db_with_user.get('username')
+
+
+def test_get_single_post_not_exist(test_db_with_user, server_address, monkeypatch):
+    monkeypatch.setattr("socialserver.api.v2.user.db", test_db_with_user.get('db'))
+    monkeypatch.setattr("socialserver.api.v2.post.db", test_db_with_user.get('db'))
+    monkeypatch.setattr("socialserver.util.auth.db", test_db_with_user.get('db'))
+
+    r = requests.get(f"{server_address}/api/v2/post/single",
+                     json={
+                         "access_token": test_db_with_user.get("access_token"),
+                         # we're on a blank database. 1 shouldn't exist.
+                         "post_id": 1
+                     })
+
+    assert r.status_code == 404
+    assert r.json()['error'] == ErrorCodes.POST_NOT_FOUND.value
+
+
+def test_get_single_post_invalid_access_token(test_db_with_user, server_address, monkeypatch):
+    monkeypatch.setattr("socialserver.api.v2.user.db", test_db_with_user.get('db'))
+    monkeypatch.setattr("socialserver.api.v2.post.db", test_db_with_user.get('db'))
+    monkeypatch.setattr("socialserver.util.auth.db", test_db_with_user.get('db'))
+
+    new_post_id = create_post_with_request(server_address,
+                                           test_db_with_user.get('access_token'))
+
+    r = requests.get(f"{server_address}/api/v2/post/single",
+                     json={
+                         "access_token": "invalid token",
+                         "post_id": new_post_id
+                     })
+
+    assert r.status_code == 401
+    assert r.json()['error'] == ErrorCodes.TOKEN_INVALID.value
+
+
+def test_get_single_post_missing_args(test_db_with_user, server_address, monkeypatch):
+    monkeypatch.setattr("socialserver.api.v2.user.db", test_db_with_user.get('db'))
+    monkeypatch.setattr("socialserver.api.v2.post.db", test_db_with_user.get('db'))
+    monkeypatch.setattr("socialserver.util.auth.db", test_db_with_user.get('db'))
+
+    new_post_id = create_post_with_request(server_address,
+                                           test_db_with_user.get('access_token'))
+
+    r = requests.get(f"{server_address}/api/v2/post/single",
+                     json={})
+
+    assert r.status_code == 400
