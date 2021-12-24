3c17bf6b9e630ef052be5e7f0f17f99b6a56f150
niall <niall@asher.sh>
2021-12-24 20:11:34 +1300

    remove the excess stuff from cli test util that's been moved over to conftest.py

--- socialserver/cli/test.py
+++ socialserver/cli/test.py
@@ -1,30 +1,6 @@
-from socialserver import application
 import pytest
-from socialserver.util.output import console
-from threading import Thread
-from werkzeug.serving import make_server
-
-
-class TestingServer(Thread):
-
-    def __init__(self, application_object):
-        Thread.__init__(self)
-        self.server = make_server("127.0.0.1",
-                                  9801,
-                                  application_object)
-        self.ctx = application_object.app_context()
-        self.ctx.push()
-
-    def run(self):
-        console.log("Starting test server.")
-        self.server.serve_forever()
-
-    def kill(self):
-        console.log("Killing test server.")
-        self.server.shutdown()
 
 
 def run_tests():
     pytest.main(["socialserver"])
-    console.log("Tests complete.")
     exit()
