3cb010356305652d784558f1e4eb72c3ff10914b
niall <niall@asher.sh>
2021-12-28 20:17:23 +1300

    add test for username_available with invalid username given

--- socialserver/tests/api2/test_username_available.py
+++ socialserver/tests/api2/test_username_available.py
@@ -1,9 +1,10 @@
 # noinspection PyUnresolvedReferences
 from socialserver.util.test import test_db, server_address
+from socialserver.constants import ErrorCodes
 import requests
 
 
-def test_username_available_name_taken(test_db, server_address, monkeypatch):
+def test_username_available_name_taken(test_db, server_address):
     r = requests.get(f"{server_address}/api/v2/user/name_available",
                      json={
                          "username": test_db.username
@@ -13,7 +14,7 @@ def test_username_available_name_taken(test_db, server_address, monkeypatch):
     assert r.json() is False
 
 
-def test_username_available_name_not_taken(test_db, server_address, monkeypatch):
+def test_username_available_name_not_taken(test_db, server_address):
     r = requests.get(f"{server_address}/api/v2/user/name_available",
                      json={
                          "username": "username"
@@ -23,7 +24,17 @@ def test_username_available_name_not_taken(test_db, server_address, monkeypatch)
     assert r.json() is True
 
 
-def test_username_available_missing_data(test_db, server_address, monkeypatch):
+def test_username_available_username_invalid(test_db, server_address):
+    r = requests.get(f"{server_address}/api/v2/user/name_available",
+                     json={
+                         "username": "invalid username"
+                     })
+
+    assert r.status_code == 400
+    assert r.json()['error'] == ErrorCodes.USERNAME_INVALID.value
+
+
+def test_username_available_missing_data(test_db, server_address):
     r = requests.get(f"{server_address}/api/v2/user/name_available", json={})
 
     assert r.status_code == 400
