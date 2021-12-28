1c2d94bb7dacbf3db4e2e63ca25123dc258f0fbc
niall <niall@asher.sh>
2021-12-28 22:25:04 +1300

    move socialserver.api.v2.follow to new auth and update test suite

--- socialserver/api/v2/follow.py
+++ socialserver/api/v2/follow.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from flask_restful import Resource, reqparse
 from socialserver.db import db
-from socialserver.util.auth import get_username_from_token
+from socialserver.util.auth import get_username_from_token, auth_reqd, get_user_from_auth_header
 from socialserver.constants import ErrorCodes
 from pony.orm import db_session
 
@@ -9,19 +9,15 @@ from pony.orm import db_session
 class Follow(Resource):
 
     @db_session
+    @auth_reqd
     def post(self):
 
         parser = reqparse.RequestParser()
-        parser.add_argument("access_token", type=str, required=True)
         # the username to follow
         parser.add_argument("username", type=str, required=True)
         args = parser.parse_args()
 
-        requesting_user = get_username_from_token(args['access_token'], db)
-        if requesting_user is None:
-            return {"error": ErrorCodes.TOKEN_INVALID.value}, 401
-
-        requesting_user_db = db.User.get(username=requesting_user)
+        requesting_user_db = get_user_from_auth_header()
 
         user_to_follow = db.User.get(username=args['username'])
         if user_to_follow is None:
@@ -45,18 +41,14 @@ class Follow(Resource):
         return {}, 201
 
     @db_session
+    @auth_reqd
     def delete(self):
 
         parser = reqparse.RequestParser()
-        parser.add_argument("access_token", type=str, required=True)
         parser.add_argument("username", type=str, required=True)
         args = parser.parse_args()
 
-        requesting_user = get_username_from_token(args['access_token'], db)
-        if requesting_user is None:
-            return {"error": ErrorCodes.TOKEN_INVALID.value}, 401
-
-        requesting_user_db = db.User.get(username=requesting_user)
+        requesting_user_db = get_user_from_auth_header()
 
         user_to_unfollow = db.User.get(username=args['username'])
         if user_to_unfollow is None:
--- socialserver/tests/api2/test_follow.py
+++ socialserver/tests/api2/test_follow.py
@@ -8,8 +8,10 @@ def test_follow_user(test_db, server_address):
     create_user_with_request(server_address, username="user2")
     r = requests.post(f"{server_address}/api/v2/follow/user",
                       json={
-                          "access_token": test_db.access_token,
                           "username": "user2"
+                      },
+                      headers={
+                          "Authorization": f"Bearer {test_db.access_token}"
                       })
     assert r.status_code == 201
 
@@ -19,8 +21,10 @@ def test_follow_user_already_followed(test_db, server_address):
     follow_user_with_request(server_address, test_db.access_token, username="user2")
     r = requests.post(f"{server_address}/api/v2/follow/user",
                       json={
-                          "access_token": test_db.access_token,
                           "username": "user2"
+                      },
+                      headers={
+                          "Authorization": f"Bearer {test_db.access_token}"
                       })
     assert r.status_code == 400
     assert r.json()['error'] == ErrorCodes.FOLLOW_ALREADY_EXISTS.value
@@ -31,8 +35,10 @@ def test_unfollow_user(test_db, server_address):
     follow_user_with_request(server_address, test_db.access_token, username="user2")
     r = requests.delete(fr"{server_address}/api/v2/follow/user",
                         json={
-                            "access_token": test_db.access_token,
                             "username": "user2"
+                        },
+                        headers={
+                            "Authorization": f"Bearer {test_db.access_token}"
                         })
     assert r.status_code == 204
 
@@ -41,8 +47,10 @@ def test_unfollow_user_not_followed(test_db, server_address):
     create_user_with_request(server_address, username="user2")
     r = requests.delete(f"{server_address}/api/v2/follow/user",
                         json={
-                            "access_token": test_db.access_token,
                             "username": "user2"
+                        },
+                        headers={
+                            "Authorization": f"Bearer {test_db.access_token}"
                         })
     assert r.status_code == 404
     assert r.json()['error'] == ErrorCodes.CANNOT_FIND_FOLLOW_ENTRY.value
@@ -51,8 +59,10 @@ def test_unfollow_user_not_followed(test_db, server_address):
 def test_try_follow_self(test_db, server_address):
     r = requests.post(f"{server_address}/api/v2/follow/user",
                       json={
-                          "access_token": test_db.access_token,
                           "username": "test"
+                      },
+                      headers={
+                          "Authorization": f"Bearer {test_db.access_token}"
                       })
     assert r.status_code == 400
     assert r.json()['error'] == ErrorCodes.CANNOT_FOLLOW_SELF.value
@@ -61,8 +71,10 @@ def test_try_follow_self(test_db, server_address):
 def test_try_follow_non_existent_user(test_db, server_address):
     r = requests.post(f"{server_address}/api/v2/follow/user",
                       json={
-                          "access_token": test_db.access_token,
                           "username": "non_existent_user"
+                      },
+                      headers={
+                          "Authorization": f"Bearer {test_db.access_token}"
                       })
     assert r.status_code == 404
     assert r.json()['error'] == ErrorCodes.USERNAME_NOT_FOUND.value
@@ -72,8 +84,10 @@ def test_follow_user_invalid_token(test_db, server_address):
     create_user_with_request(server_address, username="user1")
     r = requests.post(f"{server_address}/api/v2/follow/user",
                       json={
-                          "access_token": "invalid_token",
                           "username": "user1"
+                      },
+                      headers={
+                          "Authorization": f"Bearer invalid"
                       })
     assert r.status_code == 401
     assert r.json()['error'] == ErrorCodes.TOKEN_INVALID.value
