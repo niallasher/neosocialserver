f076391424fdbef400f98e23139e1d586ca0757d
niall <niall@asher.sh>
2022-01-11 20:04:22 +1300

    rename socialserver.api.legacy.post_filter.filter_by_user to socialserver.api.legacy.post_filter.by_user

--- socialserver/app.py
+++ socialserver/app.py
@@ -28,7 +28,7 @@ from socialserver.api.legacy.authentication import LegacyAuthentication
 from socialserver.api.legacy.info import LegacyInfo
 from socialserver.api.legacy.comment_filter.filter_by_post import LegacyCommentFilterByPost
 from socialserver.api.legacy.image import LegacyImageV1
-from socialserver.api.legacy.post_filter.filter_by_user import LegacyPostFilterByUser
+from socialserver.api.legacy.post_filter.by_user import LegacyPostFilterByUser
 from socialserver.api.legacy.like_filter.by_post import LegacyLikeFilterByPost
 from socialserver.api.legacy.follower import LegacyFollower
 from socialserver.api.legacy.block import LegacyBlock, LegacyUserBlocks
