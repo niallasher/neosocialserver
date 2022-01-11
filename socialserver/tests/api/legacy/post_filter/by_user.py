2045f7bb2d5441a63048933e13fe867defc84236
niall <niall@asher.sh>
2022-01-11 20:31:05 +1300

    add initial test_get_feed_post_for_user

---
+++ socialserver/tests/api/legacy/post_filter/by_user.py
@@ -0,0 +1,21 @@
+from socialserver.util.test import test_db, server_address, create_post_with_request, create_user_with_request, \
+    create_user_session_with_request
+import requests
+from socialserver.constants import MAX_FEED_GET_COUNT
+
+
+def test_get_post_feed_for_user(test_db, server_address):
+    create_user_with_request(server_address, username="test2", password="password")
+    user_token = create_user_session_with_request(server_address, username="test2", password="password")
+    post_id = create_post_with_request(server_address, user_token, text_content="test")
+
+    r = requests.get(f"{server_address}/api/v1/posts/byUser",
+                     json={
+                         "session_token": test_db.access_token,
+                         "count": 1,
+                         "offset": 0,
+                         "users": ["test2"]
+                     })
+
+    assert r.status_code == 201
+    assert r.json()[0] == post_id
