c73850402784219afb54adeeaa58a8618db34e38
niall <niall@asher.sh>
2022-01-11 23:54:50 +1300

    add more tests in socialserver.tests.api.legacy.post_filter.by_user.

--- socialserver/tests/api/legacy/post_filter/by_user.py
+++ socialserver/tests/api/legacy/post_filter/by_user.py
@@ -19,3 +19,67 @@ def test_get_post_feed_for_user_legacy(test_db, server_address):
 
     assert r.status_code == 201
     assert r.json()[0] == post_id
+
+
+def test_get_post_feed_for_multiple_users_legacy(test_db, server_address):
+    create_user_with_request(server_address, username="test2", password="password")
+    create_user_with_request(server_address, username="test3", password="password")
+    user_token = create_user_session_with_request(server_address, username="test2", password="password")
+    user_token_2 = create_user_session_with_request(server_address, username="test3", password="password")
+
+    post_id = create_post_with_request(server_address, user_token, text_content="test")
+    post_id_2 = create_post_with_request(server_address, user_token_2, text_content="test")
+
+    r = requests.get(f"{server_address}/api/v1/posts/byUser",
+                     json={
+                         "session_token": test_db.access_token,
+                         "count": 10,
+                         "offset": 0,
+                         "users": ["test3", "test2"],
+                     })
+
+    assert r.status_code == 201
+    # sorted by ID descending, so the first made post should be returned last
+    print(r.json())
+    assert r.json()[1] == post_id
+    assert r.json()[0] == post_id_2
+
+
+def test_get_post_feed_get_count_too_high_legacy(test_db, server_address):
+    r = requests.get(f"{server_address}/api/v1/posts/byUser",
+                     json={
+                         "session_token": test_db.access_token,
+                         "count": MAX_FEED_GET_COUNT + 1,
+                         "offset": 0,
+                         "users": ["test"]
+                     })
+    assert r.status_code == 400
+
+
+def test_get_post_feed_invalid_usernames_legacy(test_db, server_address):
+    r = requests.get(f"{server_address}/api/v1/posts/byUser",
+                     json={
+                         "session_token": test_db.access_token,
+                         "count": 10,
+                         "offset": 0,
+                         "users": ["doesntexist"]
+                     })
+    assert r.status_code == 406
+
+
+def test_get_post_feed_mixed_valid_invalid_users_legacy(test_db, server_address):
+    create_user_with_request(server_address, username="test2", password="password")
+    user_token = create_user_session_with_request(server_address, username="test2", password="password")
+    post_id = create_post_with_request(server_address, user_token, text_content="test")
+
+    r = requests.get(f"{server_address}/api/v1/posts/byUser",
+                     json={
+                         "session_token": test_db.access_token,
+                         "count": 1,
+                         "offset": 0,
+                         "users": ["test2", "invalid_user"]
+                     })
+
+    assert r.status_code == 201
+    assert len(r.json()) == 1
+    assert r.json()[0] == post_id
