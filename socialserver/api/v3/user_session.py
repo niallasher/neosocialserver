30e194ec4a227624f3ebe77750625cdd823e578b
niall <niall@asher.sh>
2022-02-12 15:34:15 +1300

    fix invalid attribute access when signing in with approval required

--- socialserver/api/v3/user_session.py
+++ socialserver/api/v3/user_session.py
@@ -55,7 +55,7 @@ class UserSession(Resource):
             return {'error': ErrorCodes.INCORRECT_PASSWORD.value}, 401
 
         if config.auth.registration.approval_required:
-            if user.approved is not True:
+            if user.account_approved is not True:
                 return {"error": ErrorCodes.ACCOUNT_NOT_APPROVED.value}, 401
 
         # if the user has totp, and hasn't specified one, send back an error
