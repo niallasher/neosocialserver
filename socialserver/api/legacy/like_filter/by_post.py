9eb7adc058f0ec4b129d8d933ba48f8cd0f45950
niall <niall@asher.sh>
2022-03-16 04:42:02 +1300

    create parser once for socialserver.api.legacy.like_filter.by_post (yes i did fumble the last few commit messages.)

--- socialserver/api/legacy/like_filter/by_post.py
+++ socialserver/api/legacy/like_filter/by_post.py
@@ -8,24 +8,24 @@ from socialserver.constants import MAX_FEED_GET_COUNT
 
 
 class LegacyLikeFilterByPost(Resource):
-    @db_session
-    def get(self):
-        parser = reqparse.RequestParser()
-
-        parser.add_argument(
+    def __init__(self):
+        self.get_parser = reqparse.RequestParser()
+        self.get_parser.add_argument(
             "session_token", type=str, help="Authentication token", required=True
         )
-        parser.add_argument(
+        self.get_parser.add_argument(
             "post_id", type=int, help="Post ID to get likes for", required=True
         )
-        parser.add_argument(
+        self.get_parser.add_argument(
             "count", type=int, help="Amount of likes to get", required=True
         )
-        parser.add_argument(
+        self.get_parser.add_argument(
             "offset", type=int, help="Offset to get from", required=True
         )
 
-        args = parser.parse_args()
+    @db_session
+    def get(self):
+        args = self.get_parser.parse_args()
 
         get_user_object_from_token_or_abort(args["session_token"])
 
@@ -39,8 +39,8 @@ class LegacyLikeFilterByPost(Resource):
         like_ids = []
         likes = (
             select(like for like in db.PostLike if like.post == post)
-                .order_by(desc(db.PostLike.id))
-                .limit(args["count"], offset=args["offset"])[:]
+            .order_by(desc(db.PostLike.id))
+            .limit(args["count"], offset=args["offset"])[:]
         )
         for like in likes:
             like_ids.append(like.id)
