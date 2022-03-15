c5f5bac12a0e9d029bba61eb5879e4179b05e16b
niall <niall@asher.sh>
2022-03-16 04:40:59 +1300

    create parser once for all files in socialserver.api.legacy.comment_filter.by_post

--- socialserver/api/legacy/comment_filter/filter_by_post.py
+++ socialserver/api/legacy/comment_filter/filter_by_post.py
@@ -7,24 +7,24 @@ from flask_restful import Resource, reqparse
 
 
 class LegacyCommentFilterByPost(Resource):
-    @db_session
-    def get(self):
-        parser = reqparse.RequestParser()
-
-        parser.add_argument(
+    def __init__(self):
+        self.get_parser = reqparse.RequestParser()
+        self.get_parser.add_argument(
             "session_token", type=str, required=True, help="Authentication token"
         )
-        parser.add_argument(
+        self.get_parser.add_argument(
             "post_id", type=int, required=True, help="Post ID to filter by"
         )
-        parser.add_argument(
+        self.get_parser.add_argument(
             "count", type=int, required=True, help="Amount of IDs to return"
         )
-        parser.add_argument(
+        self.get_parser.add_argument(
             "offset", type=int, required=True, help="Offset to filter by"
         )
 
-        args = parser.parse_args()
+    @db_session
+    def get(self):
+        args = self.get_parser.parse_args()
 
         user = get_user_object_from_token_or_abort(args["session_token"])
 
@@ -34,8 +34,8 @@ class LegacyCommentFilterByPost(Resource):
 
         comment_query = (
             select(comment for comment in db.Comment if comment.post == post)
-                .order_by(desc(db.Comment.id))
-                .limit(args["count"], offset=args["offset"])
+            .order_by(desc(db.Comment.id))
+            .limit(args["count"], offset=args["offset"])
         )
         comment_ids = []
         for comment in comment_query:
