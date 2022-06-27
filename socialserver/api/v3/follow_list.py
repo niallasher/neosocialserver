bda31fe8e8a5686af995ac542b0e1d46b973c40c
niallasher <niall@asher.sh>
2022-06-27 17:27:49 +1200

    add following list. functionally identical until helper function is updated.

--- socialserver/api/v3/follow_list.py
+++ socialserver/api/v3/follow_list.py
@@ -38,3 +38,32 @@ class FollowerList(Resource):
 
         return get_follow_info_for_user(wanted_user, count=args.count, offset=args.offset, sort_type=args.sort_type,
                                         list_type=FollowListListTypes.FOLLOWERS)
+
+
+class FollowingList(Resource):
+    def __init__(self):
+        self.get_parser = reqparse.RequestParser()
+        self.get_parser.add_argument("sort_type", type=int, required=True)
+        # we assume the user want's information about themselves if they don't specify a username
+        self.get_parser.add_argument("username", type=str, required=False)
+        self.get_parser.add_argument("count", type=int, required=True)
+        self.get_parser.add_argument("offset", type=int, required=True)
+
+    @auth_reqd
+    @db_session
+    def get(self):
+        args = self.get_parser.parse_args()
+        user = get_user_from_auth_header()
+
+        wanted_user = user
+        if args.username is not None:
+            try:
+                wanted_user = get_user_from_db(args.username)
+            except UserNotFoundException:
+                return format_error_return_v3(ErrorCodes.USERNAME_NOT_FOUND, 404)
+
+        if args.count > MAX_FEED_GET_COUNT:
+            return format_error_return_v3(ErrorCodes.FEED_GET_COUNT_TOO_HIGH, 400)
+
+        return get_follow_info_for_user(wanted_user, count=args.count, offset=args.offset, sort_type=args.sort_type,
+                                        list_type=FollowListListTypes.FOLLOWING)
