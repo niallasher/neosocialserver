d1efde2a65d3d8256725574efd0dc8b9d68f7c8c
niallasher <niall@asher.sh>
2022-07-01 13:36:00 +1200

    update v3 user to use new helpers

--- socialserver/api/v3/user.py
+++ socialserver/api/v3/user.py
@@ -10,9 +10,10 @@ from socialserver.constants import (
     MAX_PASSWORD_LEN,
     MIN_PASSWORD_LEN,
     ErrorCodes,
-    REGEX_USERNAME_VALID,
+    REGEX_USERNAME_VALID, UserNotFoundException,
 )
 from socialserver.util.api.v3.data_format import format_userdata_v3
+from socialserver.util.api.v3.error_format import format_error_return_v3
 from socialserver.util.auth import (
     generate_salt,
     hash_password,
@@ -22,6 +23,7 @@ from socialserver.util.auth import (
 )
 from pony.orm import db_session
 from socialserver.util.config import config
+from socialserver.util.user import get_user_from_db
 
 
 class UserInfo(Resource):
@@ -35,12 +37,13 @@ class UserInfo(Resource):
 
         args = self.get_parser.parse_args()
 
-        if args["username"] is None:
+        if args.username is None:
             wanted_user = get_user_from_auth_header()
         else:
-            wanted_user = db.User.get(username=args["username"])
-            if wanted_user is None:
-                return {"error": ErrorCodes.USERNAME_NOT_FOUND.value}, 404
+            try:
+                wanted_user = get_user_from_db(username=args.username)
+            except UserNotFoundException:
+                return format_error_return_v3(ErrorCodes.USERNAME_NOT_FOUND, 404)
 
         return (
             format_userdata_v3(
@@ -89,35 +92,35 @@ class User(Resource):
         # return.
 
         # usernames can only have a-z, A-Z, 0-9 or _, and must be between 1 and 20 chars
-        if not bool(re.match(REGEX_USERNAME_VALID, args["username"])):
-            return {"error": ErrorCodes.USERNAME_INVALID.value}, 400
+        if not bool(re.match(REGEX_USERNAME_VALID, args.username)):
+            return format_error_return_v3(ErrorCodes.USERNAME_INVALID, 400)
 
-        existing_user = db.User.get(username=args["username"])
+        existing_user = db.User.get(username=args.username)
         if existing_user is not None:
-            return {"error": ErrorCodes.USERNAME_TAKEN.value}, 400
+            return format_error_return_v3(ErrorCodes.USERNAME_TAKEN, 400)
 
-        if args["bio"] is not None and len(args["bio"]) >= BIO_MAX_LEN:
-            return {"error": ErrorCodes.BIO_NON_CONFORMING.value}, 400
+        if args.bio is not None and len(args.bio) >= BIO_MAX_LEN:
+            return format_error_return_v3(ErrorCodes.BIO_NON_CONFORMING, 400)
 
-        if len(args["display_name"]) >= DISPLAY_NAME_MAX_LEN:
-            return {"error": ErrorCodes.DISPLAY_NAME_NON_CONFORMING.value}, 400
+        if len(args.display_name) >= DISPLAY_NAME_MAX_LEN:
+            return format_error_return_v3(ErrorCodes.DISPLAY_NAME_NON_CONFORMING, 400)
 
-        password_ok = MIN_PASSWORD_LEN <= len(args["password"]) <= MAX_PASSWORD_LEN
+        password_ok = MIN_PASSWORD_LEN <= len(args.password) <= MAX_PASSWORD_LEN
         if not password_ok:
-            return {"error": ErrorCodes.PASSWORD_NON_CONFORMING.value}, 400
+            return format_error_return_v3(ErrorCodes.PASSWORD_NON_CONFORMING, 400)
 
         salt = generate_salt()
-        password = hash_password(args["password"], salt)
+        password = hash_password(args.password, salt)
 
         db.User(
-            display_name=args["display_name"],
-            username=args["username"],
+            display_name=args.display_name,
+            username=args.username,
             password_hash=password,
             password_salt=salt,
             creation_time=datetime.utcnow(),
             is_legacy_account=False,
             account_attributes=[],
-            bio=args["bio"] if args["bio"] is not None else "",
+            bio=args.bio if args.bio is not None else "",
             account_approved=True
             if config.auth.registration.approval_required is False
             else False,
@@ -132,47 +135,47 @@ class User(Resource):
 
         user = get_user_from_auth_header()
 
-        if args["display_name"] is not None:
-            if len(args["display_name"]) > DISPLAY_NAME_MAX_LEN:
-                return {"error": ErrorCodes.DISPLAY_NAME_NON_CONFORMING.value}, 400
-            user.display_name = args["display_name"]
-            return {"display_name": args["display_name"]}, 201
+        if args.display_name is not None:
+            if len(args.display_name) > DISPLAY_NAME_MAX_LEN:
+                return format_error_return_v3(ErrorCodes.DISPLAY_NAME_NON_CONFORMING, 400)
+            user.display_name = args.display_name
+            return {"display_name": args.display_name}, 201
 
-        if args["username"] is not None:
-            if not bool(re.match(REGEX_USERNAME_VALID, args["username"])):
-                return {"error": ErrorCodes.USERNAME_INVALID.value}, 400
-            if db.User.get(username=args["username"]) is not None:
-                return {"error": ErrorCodes.USERNAME_TAKEN.value}, 400
-            user.username = args["username"]
-            return {"username": args["username"]}
+        if args.username is not None:
+            if not bool(re.match(REGEX_USERNAME_VALID, args.username)):
+                return format_error_return_v3(ErrorCodes.USERNAME_INVALID, 400)
+            if db.User.get(username=args.username) is not None:
+                return format_error_return_v3(ErrorCodes.USERNAME_TAKEN, 400)
+            user.username = args.username
+            return {"username": args.username}
 
-        if args["bio"] is not None:
-            if len(args["bio"]) > BIO_MAX_LEN:
-                return {"error": ErrorCodes.BIO_NON_CONFORMING.value}, 400
-            user.bio = args["bio"]
+        if args.bio is not None:
+            if len(args.bio) > BIO_MAX_LEN:
+                return format_error_return_v3(ErrorCodes.BIO_NON_CONFORMING, 400)
+            user.bio = args.bio
             # NOTE: not sure; should we return new bio? client might want to cache it.
             # they could always just keep it from when it was entered, I don't know.
             return {}, 201
 
-        if args["profile_pic_ref"] is not None:
-            existing_image = db.Image.get(identifier=args["profile_pic_ref"])
+        if args.profile_pic_ref is not None:
+            existing_image = db.Image.get(identifier=args.profile_pic_ref)
             if existing_image is None:
-                return {"error": ErrorCodes.IMAGE_NOT_FOUND.value}, 404
+                return format_error_return_v3(ErrorCodes.IMAGE_NOT_FOUND, 404)
             if existing_image.processed is False:
-                return {"error": ErrorCodes.IMAGE_NOT_PROCESSED.value}, 404
+                return format_error_return_v3(ErrorCodes.IMAGE_NOT_PROCESSED, 404)
             user.profile_pic = existing_image
-            return {"profile_pic_ref": args["profile_pic_ref"]}, 201
+            return {"profile_pic_ref": args.profile_pic_ref}, 201
 
-        if args["header_pic_ref"] is not None:
-            existing_image = db.Image.get(identifier=args["header_pic_ref"])
+        if args.header_pic_ref is not None:
+            existing_image = db.Image.get(identifier=args.header_pic_ref)
             if existing_image is None:
-                return {"error": ErrorCodes.IMAGE_NOT_FOUND.value}, 404
+                return format_error_return_v3(ErrorCodes.IMAGE_NOT_FOUND, 404)
             if existing_image.processed is False:
-                return {"error": ErrorCodes.IMAGE_NOT_PROCESSED.value}, 404
+                return format_error_return_v3(ErrorCodes.IMAGE_NOT_PROCESSED, 404)
             user.header_pic = existing_image
-            return {"header_pic_ref": args["header_pic_ref"]}, 201
+            return {"header_pic_ref": args.header_pic_ref}, 201
 
-        return {"error": ErrorCodes.USER_MODIFICATION_NO_OPTIONS_GIVEN.value}, 400
+        return format_error_return_v3(ErrorCodes.USER_MODIFICATION_NO_OPTIONS_GIVEN, 400)
 
     @db_session
     @auth_reqd
@@ -182,11 +185,11 @@ class User(Resource):
         requesting_user = get_user_from_auth_header()
 
         if not verify_password_valid(
-            args["password"],
+            args.password,
             requesting_user.password_salt,
             requesting_user.password_hash,
         ):
-            return {"error": ErrorCodes.INCORRECT_PASSWORD.value}, 401
+            return format_error_return_v3(ErrorCodes.INCORRECT_PASSWORD, 401)
 
         requesting_user.delete()
         return {}, 200
