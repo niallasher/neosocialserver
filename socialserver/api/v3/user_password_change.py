c486d68a5bf438ace23ce7f6cb1c667628aaf55b
niallasher <niall@asher.sh>
2022-07-01 13:39:55 +1200

    update v3 password change to use new helpers

--- socialserver/api/v3/user_password_change.py
+++ socialserver/api/v3/user_password_change.py
@@ -3,6 +3,7 @@
 from flask_restful import Resource, reqparse
 from pony.orm import db_session
 from socialserver.constants import ErrorCodes, MIN_PASSWORD_LEN, MAX_PASSWORD_LEN
+from socialserver.util.api.v3.error_format import format_error_return_v3
 from socialserver.util.auth import (
     verify_password_valid,
     auth_reqd,
@@ -28,15 +29,15 @@ class UserPasswordChange(Resource):
 
         user = get_user_from_auth_header()
 
-        new_password = args["new_password"]
+        new_password = args.new_password
 
         if not verify_password_valid(
-                args["old_password"], user.password_salt, user.password_hash
+                args.old_password, user.password_salt, user.password_hash
         ):
-            return {"error": ErrorCodes.INCORRECT_PASSWORD.value}, 401
+            return format_error_return_v3(ErrorCodes.INCORRECT_PASSWORD, 401)
 
         if len(new_password) > MAX_PASSWORD_LEN or len(new_password) < MIN_PASSWORD_LEN:
-            return {"error": ErrorCodes.PASSWORD_NON_CONFORMING.value}, 400
+            return format_error_return_v3(ErrorCodes.PASSWORD_NON_CONFORMING, 400)
 
         new_password_salt = generate_salt()
         new_password_hash = hash_password(new_password, new_password_salt)
