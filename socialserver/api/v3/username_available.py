0dba3d5cc66d27754b97850204b125ab090fdc9d
niallasher <niall@asher.sh>
2022-07-01 13:57:37 +1200

    i'm really messing up these commit messages right now. update v3 username_available to use new helpers.

--- socialserver/api/v3/username_available.py
+++ socialserver/api/v3/username_available.py
@@ -5,6 +5,7 @@ from socialserver.db import db
 from pony.orm import db_session
 from flask_restful import Resource, reqparse
 import re
+from socialserver.util.api.v3.error_format import format_error_return_v3
 
 
 class UsernameAvailable(Resource):
@@ -16,9 +17,9 @@ class UsernameAvailable(Resource):
     def get(self):
         args = self.get_parser.parse_args()
 
-        if not bool(re.match(REGEX_USERNAME_VALID, args["username"])):
-            return {"error": ErrorCodes.USERNAME_INVALID.value}, 400
+        if not bool(re.match(REGEX_USERNAME_VALID, args.username)):
+            return format_error_return_v3(ErrorCodes.USERNAME_INVALID, 400)
 
-        existing_user = db.User.get(username=args["username"])
+        existing_user = db.User.get(username=args.username)
 
         return existing_user is None, 200
