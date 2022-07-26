742e282f82aae2b5c55aa9c9a8d78875d1420129
niallasher <niall@asher.sh>
2022-07-26 16:35:13 +1200

    add support for extra data in format_error_return_v3

--- socialserver/util/api/v3/error_format.py
+++ socialserver/util/api/v3/error_format.py
@@ -2,5 +2,9 @@
 from socialserver.constants import ErrorCodes
 
 
-def format_error_return_v3(error: ErrorCodes, return_code: int):
-    return {"error": error.value}, return_code
+def format_error_return_v3(error: ErrorCodes, return_code: int, supplemental_data: dict or None = None):
+    return_dict = {"error": error.value}
+    if supplemental_data is not None:
+        for key in supplemental_data.keys():
+            return_dict[key] = supplemental_data[key]
+    return return_dict, return_code
