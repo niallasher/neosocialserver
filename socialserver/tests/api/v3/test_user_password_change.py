452452766ec5ed3e059c12d0d2df20c44b53efb9
niallasher <niall@asher.sh>
2022-09-17 12:48:23 +1200

    add test for other session removal when changing password (if enabled)

--- socialserver/tests/api/v3/test_user_password_change.py
+++ socialserver/tests/api/v3/test_user_password_change.py
@@ -75,3 +75,50 @@ def test_change_user_password_new_password_too_long(test_db, server_address):
     )
     assert r.status_code == 400
     assert r.json()["error"] == ErrorCodes.PASSWORD_NON_CONFORMING.value
+
+
+def test_change_password_remove_other_sessions(test_db, server_address):
+    # list to store tokens; we want to loop over it and make sure
+    # they're all invalid after the password change.
+    # the first token should stay valid, since we're using it to
+    # change the password.
+    other_session_tokens = []
+    # create 10 extra sessions
+    for i in range(0, 10):
+        r = requests.post(f"{server_address}/api/v3/user/session",
+                          json={
+                              "username": test_db.username,
+                              "password": test_db.password
+                          })
+        other_session_tokens.append(r.json()['access_token'])
+    r = requests.patch(f"{server_address}/api/v3/user/password",
+                       json={
+                           "old_password": test_db.password,
+                           "new_password": "password1",  # very secure
+                           "delete_other_sessions": True
+                       },
+                       headers={"Authorization": f"bearer {test_db.access_token}"})
+    assert r.status_code == 201
+    # check that none of the other tokens work.
+    for i in other_session_tokens:
+        r = requests.patch(f"{server_address}/api/v3/user/password",
+                           json={
+                               # we want the right password here,
+                               # because we're just checking the session token
+                               "old_password": "password1",
+                               "new_password": "password2"
+                           },
+                           headers={
+                               "Authorization": f"bearer {i}"
+                           })
+        assert r.status_code == 401
+    # check that the original token still works
+    r = requests.patch(f"{server_address}/api/v3/user/password",
+                       json={
+                           "old_password": "password1",
+                           "new_password": "password2"
+                       },
+                       headers={
+                           "Authorization": f"bearer {test_db.access_token}"
+                       })
+    assert r.status_code == 201
